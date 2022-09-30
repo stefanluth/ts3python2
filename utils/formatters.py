d1dccaabfdb355d3a5fd32b2bb54244509e1b67d
stefanluth <stefan.luthardt@outlook.com>
2022-10-01 01:44:53 +0200

    add more escaping

--- utils/formatters.py
+++ utils/formatters.py
@@ -1,9 +1,11 @@
 def string_to_query(string: str | int | float) -> str:
     return (
         str(string)
-        .replace("\n", r"\n")
+        .replace("\\", r"\\")
+        .replace("/", r"\/")
         .replace(" ", r"\s")
         .replace("|", r"\p")
+        .replace("\n", r"\n")
         .replace("\t", r"\t")
         .strip()
     )
@@ -12,9 +14,11 @@ def string_to_query(string: str | int | float) -> str:
 def query_to_string(string: str | int | float):
     return (
         str(string)
-        .replace(r"\n", "\n")
+        .replace(r"\\", "\\")
+        .replace(r"\/", "/")
         .replace(r"\s", " ")
         .replace(r"\p", "|")
+        .replace(r"\n", "\n")
         .replace(r"\t", "\t")
         .strip()
     )
