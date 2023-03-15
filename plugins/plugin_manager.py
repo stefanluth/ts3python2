aa843896b989be31bb7abc34da6f8e3d6a1a919f
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-03-15 22:15:53 +0100

    add loggings

--- plugins/plugin_manager.py
+++ plugins/plugin_manager.py
@@ -11,11 +11,14 @@ logger = create_logger("PluginManager", "main.log")
 
 class PluginManager:
     def __init__(self, client: TS3Client, plugins: dict[str, dict]):
+        logger.info("Initializing plugin manager...")
         self.plugins = plugins
         self.client = client
         self.threads: dict[int, tuple[Thread, Event]] = {}
+        logger.info(f"Found {len(self.plugins)} plugins: {', '.join(self.plugins.keys())}")
 
     def run(self):
+        logger.info("Starting plugins...")
         for plugin_name, config in self.plugins.items():
             if plugin_name not in plugins.__all__:
                 logger.info(f"Plugin {plugin_name} not found. Skipping...")
