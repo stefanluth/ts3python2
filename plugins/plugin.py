443f034c8a875964222900435ebc625245b68709
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-03-17 13:20:18 +0100

    add run, stop

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -7,3 +7,9 @@ class Plugin:
     def __init__(self, client: TS3Client, event: threading.Event):
         self.client = client
         self.event = event
+
+    def run(self) -> None:
+        print("This method should be overridden by the plugin.")
+
+    def stop(self) -> None:
+        self.event.set()
