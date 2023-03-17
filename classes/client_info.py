27a0a1abd8e27986d89501eb6c7efab76dcf8674
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-03-17 02:03:57 +0100

    add Optional

--- classes/client_info.py
+++ classes/client_info.py
@@ -1,70 +1,71 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from constants import ChangeableClientProperties
 
 
 @dataclass
 class ClientInfo:
-    cid: int
-    client_idle_time: int
-    client_unique_identifier: str
-    client_nickname: str
-    client_version: str
-    client_platform: str
-    client_input_muted: int
-    client_output_muted: int
-    client_outputonly_muted: int
-    client_input_hardware: int
-    client_output_hardware: int
-    client_default_channel: str
-    client_meta_data: str
-    client_is_recording: int
-    client_version_sign: str
-    client_security_hash: str
-    client_login_name: str
-    client_database_id: int
-    client_channel_group_id: int
-    client_servergroups: int
-    client_created: int
-    client_lastconnected: int
-    client_totalconnections: int
-    client_away: int
-    client_away_message: str
-    client_type: int
-    client_flag_avatar: str
-    client_talk_power: int
-    client_talk_request: int
-    client_talk_request_msg: str
-    client_description: str
-    client_is_talker: int
-    client_month_bytes_uploaded: int
-    client_month_bytes_downloaded: int
-    client_total_bytes_uploaded: int
-    client_total_bytes_downloaded: int
-    client_is_priority_speaker: int
-    client_unread_messages: int
-    client_nickname_phonetic: str
-    client_needed_serverquery_view_power: int
-    client_default_token: str
-    client_icon_id: int
-    client_is_channel_commander: int
-    client_country: str
-    client_channel_group_inherited_channel_id: int
-    client_badges: str
-    client_myteamspeak_id: str
-    client_integrations: str
-    client_myteamspeak_avatar: str
-    client_signed_badges: str
-    client_base64HashClientUID: str
-    connection_filetransfer_bandwidth_sent: int
-    connection_filetransfer_bandwidth_received: int
-    connection_packets_sent_total: int
-    connection_bytes_sent_total: int
-    connection_packets_received_total: int
-    connection_bytes_received_total: int
-    connection_bandwidth_sent_last_second_total: int
-    connection_bandwidth_sent_last_minute_total: int
-    connection_bandwidth_received_last_second_total: int
-    connection_bandwidth_received_last_minute_total: int
-    connection_connected_time: int
-    connection_client_ip: str
+    cid: Optional[int] = None
+    client_idle_time: Optional[int] = None
+    client_unique_identifier: Optional[str] = None
+    client_nickname: Optional[str] = None
+    client_version: Optional[str] = None
+    client_platform: Optional[str] = None
+    client_input_muted: Optional[int] = None
+    client_output_muted: Optional[int] = None
+    client_outputonly_muted: Optional[int] = None
+    client_input_hardware: Optional[int] = None
+    client_output_hardware: Optional[int] = None
+    client_default_channel: Optional[str] = None
+    client_meta_data: Optional[str] = None
+    client_is_recording: Optional[int] = None
+    client_version_sign: Optional[str] = None
+    client_security_hash: Optional[str] = None
+    client_login_name: Optional[str] = None
+    client_database_id: Optional[int] = None
+    client_channel_group_id: Optional[int] = None
+    client_servergroups: Optional[int] = None
+    client_created: Optional[int] = None
+    client_lastconnected: Optional[int] = None
+    client_totalconnections: Optional[int] = None
+    client_away: Optional[int] = None
+    client_away_message: Optional[str] = None
+    client_type: Optional[int] = None
+    client_flag_avatar: Optional[str] = None
+    client_talk_power: Optional[int] = None
+    client_talk_request: Optional[int] = None
+    client_talk_request_msg: Optional[str] = None
+    client_description: Optional[str] = None
+    client_is_talker: Optional[int] = None
+    client_month_bytes_uploaded: Optional[int] = None
+    client_month_bytes_downloaded: Optional[int] = None
+    client_total_bytes_uploaded: Optional[int] = None
+    client_total_bytes_downloaded: Optional[int] = None
+    client_is_priority_speaker: Optional[int] = None
+    client_unread_messages: Optional[int] = None
+    client_nickname_phonetic: Optional[str] = None
+    client_needed_serverquery_view_power: Optional[int] = None
+    client_default_token: Optional[str] = None
+    client_icon_id: Optional[int] = None
+    client_is_channel_commander: Optional[int] = None
+    client_country: Optional[str] = None
+    client_channel_group_inherited_channel_id: Optional[int] = None
+    client_badges: Optional[str] = None
+    client_myteamspeak_id: Optional[str] = None
+    client_integrations: Optional[str] = None
+    client_myteamspeak_avatar: Optional[str] = None
+    client_signed_badges: Optional[str] = None
+    client_base64HashClientUID: Optional[str] = None
+    connection_filetransfer_bandwidth_sent: Optional[int] = None
+    connection_filetransfer_bandwidth_received: Optional[int] = None
+    connection_packets_sent_total: Optional[int] = None
+    connection_bytes_sent_total: Optional[int] = None
+    connection_packets_received_total: Optional[int] = None
+    connection_bytes_received_total: Optional[int] = None
+    connection_bandwidth_sent_last_second_total: Optional[int] = None
+    connection_bandwidth_sent_last_minute_total: Optional[int] = None
+    connection_bandwidth_received_last_second_total: Optional[int] = None
+    connection_bandwidth_received_last_minute_total: Optional[int] = None
+    connection_connected_time: Optional[int] = None
+    connection_client_ip: Optional[str] = None
