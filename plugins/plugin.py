6a843c43f39ca2cf107925df0bcde99ddf995a9b
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-03-17 15:05:48 +0100

    provide logger in property

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -2,11 +2,14 @@ import threading
 
 from ts3client import TS3Client
 
+from utils.logger import create_logger
+
 
 class Plugin:
     def __init__(self, client: TS3Client, event: threading.Event):
         self.client = client
         self.event = event
+        self.logger = create_logger(self.__class__.__name__, "main.log")
 
     def run(self) -> None:
         print("This method should be overridden by the plugin.")
